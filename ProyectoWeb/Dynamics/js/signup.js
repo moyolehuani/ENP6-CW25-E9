d9f1361774d17f5eae8c05043f1dfdf10c062409
Gael Vazquez <324256350@alumno.enp.unam.mx>
2025-06-23 09:25:14 -0600

    fix errores css menu

--- ProyectoWeb/Dynamics/js/signup.js
+++ ProyectoWeb/Dynamics/js/signup.js
@@ -41,3 +41,17 @@ submit_login.addEventListener('click', e => {
         display_signup.style.display = "none";
     }
 });
+const Op = document.querySelectorAll(".animacion_icono");
+const indicator = document.querySelector(".indicator");
+const labels = document.querySelectorAll(".icono_label");
+
+function moverIndicator(index) {
+    const label = labels[index];
+    indicator.style.top = (label.offsetTop + label.offsetHeight / 2 - indicator.offsetHeight / 2) + "px";
+    indicator.style.left = (label.offsetLeft + label.offsetWidth / 2 - indicator.offsetWidth / 2) + "px";
+}
+Op.forEach((oP, index) => {
+    oP.addEventListener('change', () => {
+        moverIndicator(index);
+    });
+});
